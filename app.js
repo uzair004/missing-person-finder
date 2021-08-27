6791846867f16e31a4f154f668a606bbc898533c
Muhammad Uzair <uzairkhattak004@gmail.com>
2021-08-27 06:59:05 +0500

    💄🩹 🚧 dummy Donation page added to avoid error

--- app.js
+++ app.js
@@ -140,6 +140,10 @@ app.get('/blog', function (req, res) {
 	}).sort({ _id: 'asc' });
 })
 
+app.get('/donate', function (req, res) {
+	res.render('donate');
+})
+
 // Start Server
 app.listen(3000, function () {
 	console.log('Server started on port 3000...');

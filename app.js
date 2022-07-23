b005b022c9851c121fd0bc1d0d03828ffe8d7e4f
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2022-07-23 17:51:00 +0500

    Update app.js: port

--- app.js
+++ app.js
@@ -160,6 +160,6 @@ app.post("/admin/login", async function(req, res, next) {
 })
 
 // Start Server
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
 	console.log('Server started on port 3000...');
 });
